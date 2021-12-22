3294ce1a1472d00dcaa2c8c22a0a0a0901802268
devjeonghwan <dev.parkjeonghwan@gmail.com>
2021-12-22 11:42:23 +0900

    Add copyrights

--- src/main/java/com/realtimetech/opack/util/structure/FastStack.java
+++ src/main/java/com/realtimetech/opack/util/structure/FastStack.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) 2021 REALTIMETECH All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
 package com.realtimetech.opack.util.structure;
 
 import org.jetbrains.annotations.NotNull;
