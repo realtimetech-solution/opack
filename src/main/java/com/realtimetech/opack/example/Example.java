7822c0f822feeb5ce48e56aab78440354cca63dc
devjeonghwan <dev.parkjeonghwan@gmail.com>
2021-12-22 11:46:44 +0900

    Change copyright format to apache license 2.0

--- src/main/java/com/realtimetech/opack/example/Example.java
+++ src/main/java/com/realtimetech/opack/example/Example.java
@@ -1,7 +1,23 @@
 /*
  * Copyright (C) 2021 REALTIMETECH All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
+ *
+ * Licensed either under the Apache License, Version 2.0, or (at your option)
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation (subject to the "Classpath" exception),
+ * either version 2, or any later version (collectively, the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.gnu.org/licenses/
+ *     http://www.gnu.org/software/classpath/license.html
+ *
+ * or as provided in the LICENSE file that accompanied this code.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.realtimetech.opack.example;
