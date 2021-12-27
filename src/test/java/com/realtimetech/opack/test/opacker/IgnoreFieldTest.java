d69fcd189eb7c64be814130a9b717f32c825a288
rodamin <qazxc5735@gmail.com>
2021-12-27 17:26:54 +0900

    Add Ignore annotation and WrapListTransformer test

---
+++ src/test/java/com/realtimetech/opack/test/opacker/IgnoreFieldTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2021 REALTIMETECH All Rights Reserved
+ *
+ * Licensed either under the Apache License, Version 2.0, or (at your option)
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation (subject to the "Classpath" exception),
+ * either version 2, or any later version (collectively, the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.gnu.org/licenses/
+ *     http://www.gnu.org/software/classpath/license.html
+ *
+ * or as provided in the LICENSE file that accompanied this code.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.realtimetech.opack.test.opacker;
+
+import com.realtimetech.opack.Opacker;
+import com.realtimetech.opack.annotation.Ignore;
+import com.realtimetech.opack.exception.DeserializeException;
+import com.realtimetech.opack.exception.SerializeException;
+import com.realtimetech.opack.test.OpackAssert;
+import com.realtimetech.opack.value.OpackValue;
+import org.junit.jupiter.api.Test;
+
+public class IgnoreFieldTest {
+    public static class IgnoreFieldTestClass {
+        @Ignore
+        private String ignoredField;
+        private String notIgnoredField;
+
+        public IgnoreFieldTestClass() {
+            this.ignoredField = "This is ignored field";
+            this.notIgnoredField = "This is not ignored field";
+        }
+    }
+
+    @Test
+    public void test() throws InstantiationException, SerializeException, DeserializeException, OpackAssert.AssertException {
+        Opacker opacker = new Opacker.Builder().create();
+        IgnoreFieldTestClass originalObject = new IgnoreFieldTestClass();
+
+        OpackValue serialized = opacker.serialize(originalObject);
+        IgnoreFieldTestClass deserialized = opacker.deserialize(IgnoreFieldTestClass.class, serialized);
+
+        OpackAssert.assertEquals(originalObject, deserialized);
+
+        if (deserialized.ignoredField != null) {
+            throw new OpackAssert.AssertException("The IgnoreFieldTestClass.ignoredField field was not ignored");
+        }
+    }
+
+}
